e7a63e3a78214472a22c0d53a8f19262b4b38663
Raja Shekar Reddy Siriganagari <siriganagari.r@northeastern.edu>
2023-10-03 18:27:07 -0400

    Modify shell script and update .gitignore

--- install_db.sh
+++ install_db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Check if the required environment variables are set
-if [[ -z "$PG_USERNAME" || -z "$PG_PASSWORD" ]]; then
-    echo "Error: Please set PG_USERNAME and PG_PASSWORD environment variables."
+if [[ -z "$POSTGRES_USER" || -z "$POSTGRES_PASSWORD" ]]; then
+    echo "Error: Please set POSTGRES_USER and POSTGRES_PASSWORD environment variables."
     exit 1
 fi
 
@@ -11,14 +11,14 @@ sudo apt update
 sudo apt install postgresql postgresql-contrib
 
 # Check if the user already exists
-if sudo -u postgres psql -t -c "SELECT 1 FROM pg_roles WHERE rolname='$PG_USERNAME'" | grep -q 1; then
+if sudo -u postgres psql -t -c "SELECT 1 FROM pg_roles WHERE rolname='$POSTGRES_USER'" | grep -q 1; then
     # User already exists, so alter the user's password
-    sudo -u postgres psql -c "ALTER ROLE $PG_USERNAME WITH PASSWORD '$PG_PASSWORD';"
-    echo "User '$PG_USERNAME' password updated."
+    sudo -u postgres psql -c "ALTER ROLE $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
+    echo "User '$POSTGRES_USER' password updated."
 else
     # User does not exist, create the PostgreSQL user with the specified username and password
-    sudo -u postgres psql -c "CREATE USER $PG_USERNAME WITH PASSWORD '$PG_PASSWORD';"
-    echo "User '$PG_USERNAME' created."
+    sudo -u postgres psql -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
+    echo "User '$POSTGRES_USER' created."
 fi
 
 
